ec14aad1198d38c09da073d7c958749a8c2129d5
Sameera Samarasinghe (Sam) <csoft.sameera@gmail.com>
2025-10-17 13:00:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-745584730-4e943",
+  "appId": "1:887534254229:web:87e7efb6634c8946c5c39c",
+  "apiKey": "AIzaSyBFRXEtqjqIZoWbwQT5HfbM80l7K3R09DI",
+  "authDomain": "studio-745584730-4e943.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "887534254229"
+};
