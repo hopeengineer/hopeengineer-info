a77d701a947ace503581472c48f6e10b90618fdd
Sameera Samarasinghe (Sam) <csoft.sameera@gmail.com>
2025-10-17 14:49:09 +0000

    Try fixing this error: `Console Error: TypeError: Cannot read properties

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,11 +1,27 @@
 import * as admin from 'firebase-admin';
+import { GoogleAuth } from 'google-auth-library';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function getFirebaseAdminSdks() {
   if (admin.apps.length === 0) {
-    // When running in a managed environment like App Hosting,
-    // initializeApp() can discover credentials and config automatically.
-    admin.initializeApp();
+    // When running in a managed environment, we can use application default credentials.
+    // However, to make this robust across all environments, we explicitly create credentials.
+    const auth = new GoogleAuth({
+      scopes: [
+        'https://www.googleapis.com/auth/cloud-platform',
+        'https://www.googleapis.com/auth/datastore',
+        'https://www.googleapis.com/auth/devstorage.full_control',
+        'https://www.googleapis.com/auth/firebase',
+        'https://www.googleapis.com/auth/identitytoolkit',
+        'https://www.googleapis.com/auth/userinfo.email',
+      ],
+    });
+
+    const credential = new admin.credential.GoogleAuth(auth);
+    
+    admin.initializeApp({
+      credential,
+    });
   }
 
   return {
