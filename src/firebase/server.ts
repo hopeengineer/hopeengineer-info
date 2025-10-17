e56b3624c4a131e84277ea3754ef7f95dc49e4bc
Sameera Samarasinghe (Sam) <csoft.sameera@gmail.com>
2025-10-17 14:43:32 +0000

    Try fixing this error: `Console Error: TypeError: Cannot read properties

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,12 +1,12 @@
+'use server';
+
 import * as admin from 'firebase-admin';
-import { firebaseConfig } from './config';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function getFirebaseAdminSdks() {
   if (admin.apps.length === 0) {
     admin.initializeApp({
       credential: admin.credential.applicationDefault(),
-      ...firebaseConfig,
     });
   }
 
