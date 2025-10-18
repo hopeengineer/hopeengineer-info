42b6b9e00c745624404f3b96c483b02d123fd392
Sameera Samarasinghe (Sam) <csoft.sameera@gmail.com>
2025-10-18 09:16:05 +0000

    no i want it to be same as previous my services size small

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -25,7 +25,7 @@ const Testimonials = () => {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">Testimonials</div>
-            <h2 className="text-3xl font-headline font-bold tracking-tighter sm:text-5xl">What My Clients Say</h2>
+            <h2 className="text-3xl font-headline font-bold">What My Clients Say</h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Authentic feedback from people who have transformed their work and life with my help.
             </p>
